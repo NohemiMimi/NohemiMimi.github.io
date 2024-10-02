b5434eefedf139a934c3ce7291d40c049ed155c4
Nohemi <nohemi.garcia.velasco20@gmail.com>
2024-10-01 21:47:11 -0600

    Ubicacion Marcada

--- static_map.js
+++ static_map.js
@@ -4,9 +4,15 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: 21.814326449344794, lng: -102.76968280340627 },
-    zoom: 15,
+    center: { lat: 21.85338845158649, lng: -102.7298880974889 },
+    zoom: 18,
+  });
+
+  new google.maps.Marker({
+    position: map.getCenter(),
+    map,
   });
 }
 
 initMap();
+
